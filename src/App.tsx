0d541178b4f0de53041c680ef3d90dc543b0a000
bruceliu <bruceliu1107@gmail.com>
2023-07-31 09:38:14 +0800

    add mui dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
+import { ChangeEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+type optionType = {label: string, value: string}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState('')
+  const [inputX, setInputX] = useState('')
+  const [inputY, setInputY] = useState('')
+  const [operator, setOperator] = useState('')
+
+  const handleInputXChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputX(event.target.value)
+  }
+  const handleInputYChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputY(event.target.value)
+  }
+  const handleInputOpChange = (selected?: optionType | null) => {
+    if(selected) {
+      setOperator(selected.value)
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input value={inputX} onChange={handleInputXChange} className='xInput'></input>
+      <select >
+        <option value='add'>
+          +
+        </option>
+        <option value='minus'>
+          -
+        </option>
+        <option value='multiply'>
+          *
+        </option>
+        <option value='divide'>
+          /
+        </option>
+      </select>
     </>
   )
 }
