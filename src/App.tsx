e49c606b243c798d562a7f3f68f9935e66caff97
bruceliu <bruceliu1107@gmail.com>
2023-07-31 10:24:47 +0800

    use of material ui dropdown selector

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { ChangeEvent, useState } from 'react'
 import reactLogo from './assets/react.svg'
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import Box, { BoxProps } from '@mui/material/Box';
 import './App.css'
 
 type optionType = {label: string, value: string}
@@ -16,14 +21,41 @@ function App() {
   const handleInputYChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputY(event.target.value)
   }
-  const handleInputOpChange = (selected?: optionType | null) => {
-    if(selected) {
-      setOperator(selected.value)
-    }
+  const handleInputOpChange = (event: SelectChangeEvent) => {
+      setOperator(event.target.value)
   }
 
   return (
     <>
+    <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent:'center',
+          alignItems: 'center', 
+          m:'2',
+          rowGap: '10px'
+        }}>
+      <TextField
+          required
+          id="outlined-required"
+          placeholder="input value X"
+          onChange={handleInputXChange}
+        />
+      <div className='operation-select'>
+        <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            label="Operator" 
+            onChange={handleInputOpChange}
+            >
+              <MenuItem value={'add'}>*</MenuItem>
+              <MenuItem value={'minus'}>-</MenuItem>
+              <MenuItem value={'multiply'}>*</MenuItem>
+              <MenuItem value={'divid'}>/</MenuItem>
+        </Select>
+      </div>
+      
       <input value={inputX} onChange={handleInputXChange} className='xInput'></input>
       <select >
         <option value='add'>
@@ -39,6 +71,7 @@ function App() {
           /
         </option>
       </select>
+      </Box>
     </>
   )
 }
