da1465b7f5500fb624c7b7fc7470871e76ff3fb9
bruceliu <bruceliu1107@gmail.com>
2023-08-01 13:59:25 +0800

    ref: use hook to handle style props

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import Select, { SelectChangeEvent } from '@mui/material/Select';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import {fetchCalculatorResult, validityCheck} from './util.ts'
+import {useHook} from './hook.ts'
 import './App.css'
 
 
 
 function App() {
-  const [count, setCount] = useState('')
   const [inputX, setInputX] = useState('')
   const [inputY, setInputY] = useState('')
   const [operator, setOperator] = useState('')
   const [result, setResult] = useState('')
   const [isInputValid, setIsInputValid] = useState(true)
   const [inputValidationMsg, setInputValidationMsg] = useState('')
+  const {boxStyleProps, xYTextFieldStyleProps, operatorTextFieldStyleProps} = useHook();
 
   const handleInputXChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputX(event.target.value)
@@ -43,14 +44,7 @@ function App() {
   return (
     <>
     <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent:'center',
-          alignItems: 'center', 
-          m:'2',
-          rowGap: '10px'
-        }}>
+        sx={boxStyleProps}>
       <TextField
           id="xInput"
           type='tel'
@@ -61,17 +55,13 @@ function App() {
             width: "80px",
             marginRight:'15px'
           }}
-          suppressContentEditableWarning={true}
         />
       <div className='operation-select'>
         <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
-            onChange={handleInputOpChange}
-            sx={{
-              width:'60px',
-              marginRight:'15px'
-            }}
+            onChange={ handleInputOpChange }
+            sx={ xYTextFieldStyleProps }
             >
               <MenuItem value={'add'}>+</MenuItem>
               <MenuItem value={'minus'}>-</MenuItem>
@@ -84,11 +74,8 @@ function App() {
           id="yInput"
           placeholder="input Y"
           label="Y"
-          onChange={handleInputYChange}
-          sx = {{
-            width: "80px",
-            marginRight:'20px'
-          }}
+          onChange={ handleInputYChange }
+          sx = { xYTextFieldStyleProps }
         />
       <p className='equal-sign' >=</p>
       <TextField 
@@ -97,11 +84,7 @@ function App() {
         value={result}
         label='result'
         type='tel'
-        sx={{
-          input: {textAlign: "center"},
-          width: "90px"
-        }} />
-        
+        sx={operatorTextFieldStyleProps} />
       </Box>
       <Button variant="contained" onClick={getCalculationResult}>Get Result</Button>
       {
