9b1a0cc5e4e10efaf3d96456a00fb848f24796b3
bruceliu <bruceliu1107@gmail.com>
2023-08-05 15:47:50 +0800

    fix: respond time color

--- src/components/NetworkRespondTime/hook.ts
+++ src/components/NetworkRespondTime/hook.ts
@@ -1,6 +1,6 @@
 const useNetworkRespondTime = (apiFetchTime: number) => {
-    if (apiFetchTime < 400) return {color: '#1c950f'}
-    if (apiFetchTime < 800) return {color: '#c5820e'}
+    if (apiFetchTime < 600) return {color: '#1c950f'}
+    if (apiFetchTime < 2000) return {color: '#c5820e'}
     return {color: '#c5140e'}
 }
 
