366c2a59b990ce5ce30a04101f8c968bffc52440
bruceliu <bruceliu1107@gmail.com>
2023-08-05 09:54:13 +0800

    feat: cloud calculator hover effect

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,21 @@
 
-import { Box, IconButton } from '@mui/material'
+import { Box, IconButton, Link } from '@mui/material'
 import useHeader from './hook'
 import { FaGithub } from 'react-icons/fa';
+import './style.css'
 function Header(){
     const { headerStyleProps } = useHeader()
   return (  
     <>
         <Box sx={headerStyleProps}>
-            <img src='./logo.png' height={'70px'}/>
-            <IconButton size="small" >
-                <FaGithub className="svg_icons" size = {'2em'}/>
-            </IconButton>
+            <div className='imgContainer'>
+                <img src='./logo.png' height={'70px'} className='logoImg'/>
+            </div>
+            <Link>
+                <IconButton size="small" sx={{padding: 1, marginRight: 1}} >
+                    <FaGithub className="svg_icons" size = {'2em'}/>
+                </IconButton>
+            </Link>
         </Box>
         
     </>
